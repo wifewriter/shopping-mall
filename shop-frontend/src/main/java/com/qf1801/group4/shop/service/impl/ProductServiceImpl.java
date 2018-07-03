5a34400a11f5590531b239c7b1b0aaa8ce6c3f5e
VickTyou <1038319026@qq.com>
2018-07-03 17:49:26 +0800

    开始实现主页展示

--- shop-frontend/src/main/java/com/qf1801/group4/shop/service/impl/ProductServiceImpl.java
+++ shop-frontend/src/main/java/com/qf1801/group4/shop/service/impl/ProductServiceImpl.java
@@ -1,7 +1,9 @@
 package com.qf1801.group4.shop.service.impl;
 
+import com.qf1801.group4.shop.dao.ShopProductMapper;
 import com.qf1801.group4.shop.entity.ShopProduct;
 import com.qf1801.group4.shop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,8 +11,13 @@ import java.util.Map;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ShopProductMapper shopProductMapper;
+
     @Override
     public List<ShopProduct> get(Map<String, Object> params) {
+
         return null;
     }
 }
