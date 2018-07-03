3970308ae9aec7b524a4eb4db61f59756726cc0a
VickTyou <1038319026@qq.com>
2018-07-03 17:38:30 +0800

    开始实现主页展示

---
+++ shop-frontend/src/main/java/com/qf1801/group4/shop/service/impl/ProductServiceImpl.java
@@ -0,0 +1,16 @@
+package com.qf1801.group4.shop.service.impl;
+
+import com.qf1801.group4.shop.entity.ShopProduct;
+import com.qf1801.group4.shop.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public List<ShopProduct> get(Map<String, Object> params) {
+        return null;
+    }
+}
